a4e8d8efc88bdd1ae1e09cb2d8bd0778d484d6b0
KrishnaPermal <krishna.tantalidespermalnaiken@gmail.com>
2019-09-19 14:35:32 +0400

    third commit

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
-let mymap = L.map('map').setView([-21.115141,55.536384], 10);
+let mymap = L.map('map', {
+	maxBounds: [				//maxBounds bloque la maps à un endroit précis (sud est nord est)
+		//south west
+		[-21.39938, 55.17609],
+		//north east
+		[-20.84598, 55.82977]
+	],
+}).setView([-21.115141,55.536384], 10);
+
 
 
 //Charger une seule zone de la map
-/* let corner1 = L.latLng(-21.096192,55.310154);
+let corner1 = L.latLng(-21.096192,55.310154);
 let corner2 = L.latLng(-21.112207,55.284576);
 let bounds = L.latLngBounds(corner1, corner2);
 
-//les pointeurs à mettre en dessous de addTo
+/*
+//les pointeurs à mettre en dessous de addTo (CARTE D'ORIGINE)
 let firstMarker = L.marker(corner1).addTo(mymap);
 let secondMarker = L.marker(corner2).addTo(mymap);*/ 
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+
+//CARTE D'ORIGINE
+/*  L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
-	/* bounds: bounds, */ // zoom sur une zone de la map
+	/* bounds: bounds,  // zoom sur une zone de la map
 	id: 'mapbox.streets',
 	accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
-
+}).addTo(mymap);  */
 
 
 
@@ -58,3 +68,79 @@ mymap.locate({setView: true, watch: true}) /* This will return map so you can do
 			});
 let marker = L.marker([51.5, -0.09]).addTo(mymap);
  */
+
+//CHANGEMENT DES CARTES//
+
+// Tile type: openstreetmap normal
+let openstreetmap = L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>',
+  	maxZoom: 18,
+	bounds: bounds, // zoom sur une zone de la map
+	accessToken: 'your.mapbox.access.token'
+})	.addTo(mymap); 
+
+
+// Tile type: openstreetmap Hot
+let openstreetmapHot = L.tileLayer(
+  'http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>',
+  	maxZoom: 18,
+	bounds: bounds,  // zoom sur une zone de la map
+	accessToken: 'your.mapbox.access.token'
+})	.addTo(mymap); 
+
+
+// Tile type: openstreetmap Osm
+let openstreetmapOsm = L.tileLayer(
+  'http://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>',
+  	maxZoom: 18,
+	//bounds: bounds, // zoom sur une zone de la map
+	accessToken: 'your.mapbox.access.token'
+})	.addTo(mymap); 
+
+
+//Base layers definition and addition
+let allOptions = {
+	"Open streetmap": openstreetmap,
+  	"Open streetmap: Hot": openstreetmapHot,
+  	"Open streetmap: Osm": openstreetmapOsm
+};
+
+// Initialize with openstreetmap
+openstreetmap.addTo(mymap);
+
+// Add baseLayers to map as control layers
+L.control.layers(allOptions).addTo(mymap);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//EXEMPLE SI ONT VEUT METTRE UN BOUTON CLIQUE
+/* let coucheUn = document.querySelector("#couche1");
+//La couche relier à la div :
+coucheUn.onclick = function() {
+    couche = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright%22%3EOpenStreetMap</a> contributors',
+    }).addTo(mymap);
+} */
